a8184e00f27494048c72bd216ebf6decff144281
Phil-Chuong <Phil-Chuong@users.noreply.github.com>
2024-10-25 09:52:04 +0100

    test 6

--- backend/DB/db.js
+++ backend/DB/db.js
@@ -2,7 +2,7 @@
 const { Pool } = require('pg');
 const config = require('../config');
 
-const pool = new Pool(config.DB);
+const pool = new Pool(config.DB_CONFIG);
 console.log('Connected to PGadmin Database');
 
 module.exports = pool;

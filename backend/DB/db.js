4a47dba68bd23855aa1e8cf26c741ab05be35d95
Phil-Chuong <Phil-Chuong@users.noreply.github.com>
2024-10-30 17:04:57 +0000

    Using PG connection

--- backend/DB/db.js
+++ backend/DB/db.js
@@ -3,9 +3,8 @@ require('dotenv').config();
 const { Pool } = require('pg');
 const config = require('../config');
 
-const pool = new Pool( config.DB_CONFIG );
+const pool = new Pool( config.DB);
 
-
-console.log('Connecting to:', config.DB_CONFIG);
+console.log('Connecting to:', config.DB);
 
 module.exports = pool;
